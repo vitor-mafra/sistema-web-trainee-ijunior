2d7800ded04c714904339a3740da0fa3ee694038
Vitor Mafra <vitor.mafra@hotmail.com>
2018-11-01 15:56:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,31 @@
-
-<!-- Menu 
+<!--
+Menu 
 Página Inicial 
 Lojas 
-Discos-->
+Discos
+-->
 <!DOCTYPE html>
 <html>
     <meta charset="UTF-8">
     <head>
-            <title>Menu Principal</title>
+            <title>Pearl Records© 2018</title>
             <link rel="stylesheet" href="Menu.css" type "text/css">
     </head>
     <body>
         <header>
             <nav class="menu">
                 <ul>
-                    <li><a href = "index.php">Página Inicial</a></li>
+                    <li><a href = "index.php">Home</a></li>
                     <li><a href = "listarLojas.php">Lojas</a></li>
                     <li><a href = "listarDiscos.php">Discos</a></li>
                 </ul>
             </nav>
         </header>
         <div>
-            <img src="LOGO.jpg" alt = "Pearl Records.inc" height = 500px width = 600px>
+            <img src="logo_pearl_records.jpg" alt = "Pearl Records.inc" height = 500px width = 600px>
         </div>
         <footer>
-            <h4>Pearl Records 2018</h4>
+            <h4>Pearl Records© 2018</h4>
         </footer>
     </body>
 </html>
