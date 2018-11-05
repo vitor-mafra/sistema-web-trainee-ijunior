fd9cca847555da52abc154fed8e2ea72a8fb3162
Fernanda Rocha <42345598+fernandagonc@users.noreply.github.com>
2018-11-05 16:26:31 -0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,31 +1,29 @@
-<!--
-Menu 
+<!-- Menu 
 Página Inicial 
 Lojas 
-Discos
--->
+Discos-->
 <!DOCTYPE html>
 <html>
     <meta charset="UTF-8">
     <head>
-            <title>Pearl Records© 2018</title>
+            <title>Menu Principal</title>
             <link rel="stylesheet" href="Menu.css" type "text/css">
     </head>
     <body>
         <header>
             <nav class="menu">
                 <ul>
-                    <li><a href = "index.php">Home</a></li>
+                    <li><a href = "index.php">Página Inicial</a></li>
                     <li><a href = "listarLojas.php">Lojas</a></li>
                     <li><a href = "listarDiscos.php">Discos</a></li>
                 </ul>
             </nav>
         </header>
         <div>
-            <img src="logo_pearl_records.jpg" alt = "Pearl Records.inc" height = 500px width = 600px>
+            <img src="logo.jpg" alt="Pearl Records.inc" style = "height:500px width = 600px">
         </div>
         <footer>
-            <h4>Pearl Records© 2018</h4>
+            <h4>Pearl Records 2018</h4>
         </footer>
     </body>
 </html>
