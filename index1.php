2190956b4d93506e6aac3f29ad86d8671554d15c
GiThiago <42196167+GiThiago@users.noreply.github.com>
2018-11-05 18:11:25 -0200

    Add files via upload
    Lembrar de mudar o nome desse arquivo para index.php (retirar esse 1)

---
+++ index1.php
@@ -0,0 +1,31 @@
+<!--
+Menu 
+Página Inicial 
+Lojas 
+Discos
+-->
+<!DOCTYPE html>
+<html>
+    <meta charset="UTF-8">
+    <head>
+            <title>Pearl Records© 2018</title>
+            <link rel="stylesheet" href="Menu.css" type "text/css">
+    </head>
+    <body>
+        <header>
+            <nav class="menu">
+                <ul>
+                    <li><a href = "index.php">Home</a></li>
+                    <li><a href = "listarLojas.php">Lojas</a></li>
+                    <li><a href = "listarDiscos.php">Discos</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div>
+            <img class = "img" src="logo_pearl_records.jpg" alt = "Pearl Records.inc" height = 500px width = 600px>
+        </div>
+        <footer>
+            <h4>Pearl Records© 2018</h4>
+        </footer>
+    </body>
+</html>
